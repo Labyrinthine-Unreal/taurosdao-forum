fdcd1857124b07fc3c28019a4a4e705aac1059ca
Calvin Green <calvingreen94@aol.com>
2023-04-27 13:14:23 -0500

    Login

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+import { ClerkProvider } from "@clerk/nextjs/app-beta";
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <title>Next.js 13 with Clerk</title>
+        </head>
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ page.jsx
@@ -0,0 +1,10 @@
+import { currentUser } from "@clerk/nextjs/app-beta"
+
+export async function GET() {
+  const user = await currentUser();
+  if (!user) {
+    return new Response('Hello, world!')
+  }
+
+  return new Response(`Hello, ${user.firstName}!`)
+}
