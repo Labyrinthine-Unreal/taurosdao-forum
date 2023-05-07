f131287cdb555119fd6374c98c4a0d8ffabce10a
Calvin Green <calvingreen94@aol.com>
2023-05-07 18:05:26 -0500

    update username fauna gql

--- src/components/TopicList.js
+++ src/components/TopicList.js
@@ -4,6 +4,7 @@ import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
 import parse from 'html-react-parser';
 import styles from './TopicList.module.css';
+import { ClerkProvider, useUser, SignIn, SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/nextjs'
 
 const ITEMS_QUERY = gql`
 query MyTopicQuery {
@@ -41,7 +42,8 @@ console.log(ITEMS_QUERY)
 export default function TopicList() {
  const { data, loading, error } = useQuery(ITEMS_QUERY);
  console.log(data)
-
+ const { user } = useUser()
+ 
  if (loading) return 'Loading...';
  if (error) return `Error! ${error.message}`;
 
@@ -52,7 +54,7 @@ export default function TopicList() {
           {data.topics_by_slug.data.map((item) => {
               return (
                 <div key={item.id} className={styles.topicItem}>
-                <div className={styles.topicTitle}>{item.topic}</div> 
+                <div className={styles.topicTitle}>{item.topic}:{user.username}</div> 
                 <div className={styles.topicContent}>{parse(item.content)}</div>
                 </div>
               );
