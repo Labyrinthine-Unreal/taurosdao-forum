c02a1b86c3616394ab4b6f37350863f5f9e5bee8
tectumor <72674429+tectumor@users.noreply.github.com>
2023-05-24 21:22:39 -0500

    display comments

--- src/components/commentComponents/CommentList.js
+++ src/components/commentComponents/CommentList.js
@@ -1,16 +1,15 @@
 // src/components/commentComponents/CommentList.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useQuery, gql } from '@apollo/client';
 import { useUser } from '@clerk/nextjs';
 import parse from 'html-react-parser';
 import Header from '@root/components/layout/Header';
-// import styles from './TopicPage.module.css';
 import UpdateTopic from '@root/components/topicComponents/UpdateTopic';
 import faunadb from 'faunadb';
-// import CommentList from '@root/components/commentComponents/CommentList';
 import { CSSTransition } from 'react-transition-group';
 import ReplyButton from '@root/components/buttons/ReplyButton';
+
 const q = faunadb.query;
 const client = new faunadb.Client({ secret: "fnAFDZGm3pAASZlfCHemrt0fvXUPK1gb0ZqnbR6f", keepAlive: true });
 
@@ -27,85 +26,63 @@ query MyTopicQuery($slug: String!){
   }
 `;
 
-
-// console.log(ITEMS_QUERY)
-
 const CommentList = () => {
   const router = useRouter();
   const { slug } = router.query;
   const { data, loading, error } = useQuery(GET_TOPIC_BY_SLUG, {
     variables: { slug },
     skip: !slug,
-    // Skip the query if the slug is not available
   })
   const { user } = useUser()
+  const [comments, setComments] = useState([]); // <-- store comments in state
+
+  useEffect(() => {
+    if (data?.topics_by_slug.slug) {
+      var createP = client.query(
+        q.Paginate(q.Match(q.Index("getCommentsBySlug"), data?.topics_by_slug.slug))
+      );
+      createP.then(async function (response) {
+        // Check if response.data is defined and is an array
+        if (Array.isArray(response.data)) {
+          response.data.map(
+            ([item, ref, comment, name, slug, date]) => {
+              console.log(comment)
+              console.log(name)
+              console.log(response.data)
+              console.log(JSON.stringify(response.data))
+              // Check if response.data[0] is defined and is an array with at least 5 elements
+              if (Array.isArray(response.data[0]) && response.data[0].length >= 5) {
+                console.log(response.data[0][3])
+                console.log(response.data[0][4])
+              }
+              // Check if response.data[1] is defined and is an array with at least 5 elements
+              if (Array.isArray(response.data[1]) && response.data[1].length >= 5) {
+                console.log(response.data[1][3])
+                console.log(response.data[1][4])
+              }
+            }
+          )
+          setComments(response.data); // <-- update comments state
+        }
+      })
+    }
+  }, [data]);
 
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
 
-  const client = new faunadb.Client({ secret: "fnAFDZGm3pAASZlfCHemrt0fvXUPK1gb0ZqnbR6f", keepAlive: true });
-  // console.log(client)
-
-  var createP = client.query(
-    q.Paginate(q.Match(q.Index("getCommentsBySlug"), data?.topics_by_slug.slug))
-
-  );
-
-  var urls = {}
-  var results = createP.then(async function (response) {
-    response.data.map(
-      ([item, ref, comment, name, slug, date]) => {
-        // item
-        // ref
-        console.log(comment)
-        console.log(name)
-        console.log(response.data)
-        console.log(JSON.stringify(response.data))
-        console.log(response.data[0][3])
-        console.log(response.data[0][4])
-        console.log(response.data[1][3])
-        console.log(response.data[1][4])
-      }
-    )
-    // console.log(JSON.stringify(results))
-  })
-  urls['data'] = results
-  console.log(urls['data'])
-  // const resultsArray = results[1]
-  // console.log(resultsArray)
-
-
-
-// const isPostID = data?.comments_by_id.forumID === data?.comments_by_id.slug
-// console.log(isPostID)
-
-return (
-  <>
-  Comments
-  <br />
-  {/* {resultsArray} */}
-  {/* {urls['data']} */}
-  {/* {results} */}
-  {JSON.stringify(createP.then(function (response) {
-    response.data.map(
-      ([item, ref, comment, name, slug, date]) => {
-        // item
-        // ref
-        comment
-        console.log(name)
-        console.log(response.data)
-        // JSON.stringify(response.data)
-        [response.data[0][3]]
-        console.log(response.data[0][4])
-        console.log(response.data[1][3])
-        console.log(response.data[1][4])
-      }
-    )
-    // console.log(JSON.stringify(results))
-  }))} 
-  
-</>
-)
+  return (
+    <>
+      Comments
+      <br />
+      {comments.map(([item, ref, comment, name, slug, date], index) => (
+  <div key={index}>
+    <p>{comment}</p>
+    <p>{name}</p>
+  </div>
+))}
+    </>
+  )
 }
 
 export default CommentList;
